b9c3fa8a426d578c237e061281591f28a58c6965
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2024-06-27 17:39:19 +0300

    done cycled list

--- data_structures/lists/cycle_linked_list.go
+++ data_structures/lists/cycle_linked_list.go
@@ -1,5 +1,7 @@
 package lists
 
+import "errors"
+
 type cycledLinkedNode[T any] struct {
 	value T
 	prev  *cycledLinkedNode[T]
@@ -42,4 +44,81 @@ func (cl *CycleLinkedList[T]) Add(element T) {
 	cl.head.prev = node
 	cl.head = node
 	cl.size++
+}
+
+var ErrListIsEmpty = errors.New("list is empty")
+
+func (cl *CycleLinkedList[T]) Value() (T, error) {
+	if cl.isEmpty() {
+		return *new(T), ErrListIsEmpty
+	}
+	return cl.head.value, nil
+}
+
+type CycleLinkFunc[T any] func(T)
+
+func (cl *CycleLinkedList[T]) ForEach(clFunc CycleLinkFunc[T]) {
+	if cl.isEmpty() {
+		return
+	}
+	node := cl.head
+	clFunc(node.value)
+	for i := cl.size; i > 1; i-- {
+		node = node.next
+		clFunc(node.value)
+	}
+}
+
+func (cl *CycleLinkedList[T]) ReverseForEach(clFunc CycleLinkFunc[T]) {
+	if cl.isEmpty() {
+		return
+	}
+	node := cl.head.prev
+	clFunc(node.value)
+	for i := cl.size; i > 1; i-- {
+		node = node.prev
+		clFunc(node.value)
+	}
+}
+
+func (cl *CycleLinkedList[T]) Rotate(delta int) {
+	if cl.isEmpty() {
+		return
+	}
+	delta %= cl.size
+	if delta == 0 {
+		return
+	}
+	if delta < 0 {
+		for ; delta < 0; delta++ {
+			cl.head = cl.head.prev
+		}
+	} else {
+		for i := 0; i < delta; i++ {
+			cl.head = cl.head.next
+		}
+	}
+}
+
+func (cl *CycleLinkedList[T]) Remove() bool {
+	if cl.isEmpty() {
+		return false
+	}
+	currentNode := cl.head
+	if cl.size == 1 {
+		cl.head = nil
+		cl.size--
+		return true
+	}
+
+	cl.head = currentNode.next
+	cl.head.prev.next = currentNode.next
+	cl.head.prev = currentNode.prev
+	cl.size--
+	return true
+}
+
+func (cl *CycleLinkedList[T]) RemoveAll() {
+	for cl.Remove() {
+	}
 }
