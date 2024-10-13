27c8a58cb3caa9bfd5e8945a7b761b7e96f4ea29
Zorin Kirill <thesecretsoftheuniverse@mail.ru>
2024-10-13 19:53:22 +0300

    in progress

--- data_structures/tree/binary_tree.go
+++ data_structures/tree/binary_tree.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Rib interface {
 	Key() int
 }
@@ -16,6 +21,10 @@ func newNode[T Rib](data T) *Node[T] {
 
 func (n *Node[T]) key() int {
 	return n.data.Key()
+}
+
+func (n *Node[T]) isLeaf() bool {
+	return n.left == nil && n.right == nil
 }
 
 type BinaryTree[T Rib] struct {
@@ -101,6 +110,100 @@ func (t *BinaryTree[T]) Maximum() (T, bool) {
 	return maximum.data, true
 }
 
+var ErrNotFoundElementByKey = errors.New("cannot find element by key")
+
 func (t *BinaryTree[T]) Remove(key int) error {
+	if t.IsEmpty() {
+		return nil
+	}
+	var parent *Node[T]
+	var isLeftNode bool
+	current := t.root
+	for current != nil {
+		if current.key() == key {
+			break
+		}
+		parent = current
+		if current.data.Key() > key {
+			current = current.left
+			isLeftNode = true
+		} else {
+			current = current.right
+		}
+	}
+	if current == nil {
+		return ErrNotFoundElementByKey
+	}
+
+	if current.isLeaf() {
+		if current == t.root {
+			t.root = nil
+		}
+		if isLeftNode {
+			parent.left = nil
+		} else {
+			parent.right = nil
+		}
+	} else if current.left == nil {
+		if current == t.root {
+			t.root = current.right
+		} else if isLeftNode {
+			parent.left = current.right
+		} else {
+			parent.right = current.right
+		}
+		current = nil
+	} else if current.right == nil {
+		if current == t.root {
+			t.root = current.left
+		} else if isLeftNode {
+			parent.left = current.left
+		} else {
+			parent.right = current.left
+		}
+		current = nil
+	} else {
+		successor := fetchSuccessor(current)
+		if t.root == current {
+			t.root = successor
+		} else if isLeftNode {
+			parent.left = successor
+		} else {
+			parent.right = successor
+		}
+	}
+
 	return nil
+}
+
+func (t *BinaryTree[T]) SymmetricTraversal(f func(T)) {
+	fmt.Println("symmetric traversal")
+	t.symmetricTraversal(t.root, f)
+	fmt.Println()
+}
+
+func (t *BinaryTree[T]) symmetricTraversal(localRoot *Node[T], f func(T)) {
+	if localRoot == nil {
+
+	}
+}
+
+func fetchSuccessor[T Rib](node *Node[T]) *Node[T] {
+	successorParent := node
+	successor := node
+	current := node.right
+	for current != nil {
+		successorParent = successor
+		successor = current
+		current = current.left
+	}
+	if successor != node.right {
+		successorParent.left = successor.right
+		successor.right = node.right
+		successor.left = node.left
+	} else {
+		successor = successorParent.left
+		successor.right = successorParent.right
+	}
+	return successor
 }
